c1304a3c48329d9058cf8fbf0b4331999f0a9450
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2025-03-01 15:48:50 +0530

    Initial Game

---
+++ src/Board/board.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED := 300.0
+const getXDir := 0
+
+func getYDir() -> float:
+	return Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+func _physics_process(delta: float) -> void:
+	var dir: Vector2 = Vector2(getXDir, getYDir())
+	velocity = dir * SPEED
+	move_and_slide()
