6a7db48788bb8c8f9109d85fb17fe0cf1c10a291
JPaul <jpp3062005@gmail.com>
2025-03-06 20:34:06 +0530

    Add Reset Function To The Ball If It Hits The Endpoints

--- src/Ball/ball.gd
+++ src/Ball/ball.gd
@@ -2,12 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 15.0
+var initial_position: Vector2
 
 func _ready() -> void:
 	velocity = Vector2(-SPEED, 0)
+	initial_position = position
 
 func _physics_process(delta: float) -> void:
 	var col: KinematicCollision2D = move_and_collide(velocity)
 	if col:
-		var normal := col.get_normal()
-		velocity = velocity.bounce(normal)
+		if col.get_collider().is_in_group("walls"):
+			reset()
+		else:
+			var normal := col.get_normal()
+			velocity = velocity.bounce(normal)
+			
+func reset() -> void:
+	position = initial_position
+	velocity = Vector2(-SPEED, 0)
